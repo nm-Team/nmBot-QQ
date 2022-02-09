b430d7c7f9d4d372a7f06cab113d661159bcd4fc
agou <2833769658@qq.com>
2022-02-09 22:30:36 +0800

    [Upd]

--- plugins/NOxyz1024.js
+++ plugins/NOxyz1024.js
@@ -5,15 +5,16 @@ require("./ad.js");
 bot.on("message", function (e) {
     if (e.sender.user_id == 486483978) {
         if (ma(e.raw_message)) {
-            bot.setGroupBan(e.group_id, 486483978, 60);
+            if (Math.round(Math.random() > 0.6))
+                bot.setGroupBan(e.group_id, 486483978, Math.round(Math.random() * 6) * 60);
             bot.deleteMsg(e.message_id);
-            e.reply("检测到嘴臭怪物 且含妈量高 自动为群友屏蔽"+ad());
+            e.reply("检测到嘴臭怪物 且含妈量高 自动为群友屏蔽" + ad());
         }
     }
 })
 
 function ma(w) {
     w = w.replace(/\[CQ:[^"$]*\]/g, "").replace(/\/nm /g, "");
-    if (w.indexOf("马") == -1 && w.indexOf("妈") == -1 && w.indexOf("🐎") == -1) return false;
+    if (w.indexOf("马") == -1 && w.indexOf("妈") == -1 && w.indexOf("🐎") == -1 && w.indexOf("🐴") == -1 && w.indexOf("ma") == -1) return false;
     else return true;
 }
